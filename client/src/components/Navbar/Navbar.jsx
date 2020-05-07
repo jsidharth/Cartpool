a8ccd4d85813c5dbb5e231133ec2a3faea944b37
Sidharth J <sidharthjayaprakash93@gmail.com>
2020-05-06 20:14:14 -0700

    Navbar changes

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,16 @@
 import React, { Component } from "react";
 import { TiShoppingCart } from "react-icons/ti";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { authActions } from "../../js/actions";
+import _ from "lodash";
 
 class Navbar extends Component {
   state = {};
+
+  signOut = () => {
+    this.props.signOut();
+  };
   render() {
     return (
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
@@ -23,95 +30,125 @@ class Navbar extends Component {
         >
           <span className="navbar-toggler-icon"></span>
         </button>
+
         <div className="collapse navbar-collapse" id="navbarNavDropdown">
-          <ul className="navbar-nav">
-            <li className="nav-item active">
-              <Link to="/browse/stores" style={{ textDecoration: "none" }}>
-                <div className="nav-link">Browse</div>
-              </Link>
-            </li>
+          {this.props.user.role === "USER" ? (
+            <ul className="navbar-nav">
+              <li className="nav-item active">
+                <Link to="/browse/stores" style={{ textDecoration: "none" }}>
+                  <div className="nav-link">Browse</div>
+                </Link>
+              </li>
 
-            <li className="nav-item dropdown">
-              <div
-                className="nav-link dropdown-toggle"
-                id="navbarDropdownMenuLink"
-                role="button"
-                data-toggle="dropdown"
-                aria-haspopup="true"
-                aria-expanded="false"
-              >
-                Pool
-              </div>
-              <div
-                className="dropdown-menu"
-                aria-labelledby="navbarDropdownMenuLink"
-              >
-                <Link className="dropdown-item" to="/pool/browse">
-                  Browse pools
-                </Link>
-                <Link className="dropdown-item" to="/pool/detail">
-                  My Pool
-                </Link>
-              </div>
-            </li>
+              <li className="nav-item dropdown">
+                <div
+                  className="nav-link dropdown-toggle"
+                  id="navbarDropdownMenuLink"
+                  role="button"
+                  data-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                >
+                  Pool
+                </div>
+                <div
+                  className="dropdown-menu"
+                  aria-labelledby="navbarDropdownMenuLink"
+                >
+                  <Link className="dropdown-item" to="/pool/browse">
+                    Browse pools
+                  </Link>
+                  <Link className="dropdown-item" to="/pool/detail">
+                    My Pool
+                  </Link>
+                </div>
+              </li>
 
-            <li className="nav-item dropdown">
-              <div
-                className="nav-link dropdown-toggle"
-                id="navbarDropdownMenuLink"
-                role="button"
-                data-toggle="dropdown"
-                aria-haspopup="true"
-                aria-expanded="false"
-              >
-                Order
-              </div>
-              <div
-                className="dropdown-menu"
-                aria-labelledby="navbarDropdownMenuLink"
-              >
-                <Link className="dropdown-item" to="">
-                  My Orders
+              <li className="nav-item dropdown">
+                <div
+                  className="nav-link dropdown-toggle"
+                  id="navbarDropdownMenuLink"
+                  role="button"
+                  data-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                >
+                  Order
+                </div>
+                <div
+                  className="dropdown-menu"
+                  aria-labelledby="navbarDropdownMenuLink"
+                >
+                  <Link className="dropdown-item" to="">
+                    My Orders
+                  </Link>
+                  <Link className="dropdown-item" to="">
+                    Orders to pickup
+                  </Link>
+                </div>
+              </li>
+            </ul>
+          ) : (
+            <ul className="navbar-nav">
+              <li className="nav-item active">
+                <Link to="/admin/stores" style={{ textDecoration: "none" }}>
+                  <div className="nav-link">Store</div>
                 </Link>
-                <Link className="dropdown-item" to="">
-                  Orders to pickup
+              </li>
+              <li className="nav-item active">
+                <Link to="/admin/products" style={{ textDecoration: "none" }}>
+                  <div className="nav-link">Products</div>
                 </Link>
+              </li>
+            </ul>
+          )}
+          {!_.isEmpty(this.props.user) ? (
+            <div className="collapse navbar-collapse justify-content-end">
+              <div className="navbar-nav">
+                <ul className="navbar-nav">
+                  <li className="nav-item dropdown dropleft">
+                    <div
+                      className="nav-link dropdown-toggle "
+                      id="navbarDropdownMenuLink"
+                      role="button"
+                      data-toggle="dropdown"
+                      aria-haspopup="true"
+                      aria-expanded="false"
+                    >
+                      {this.props.user.nickName}
+                    </div>
+
+                    <div
+                      className="dropdown-menu"
+                      aria-labelledby="navbarDropdownMenuLink"
+                    >
+                      <Link className="dropdown-item" to="/account">
+                        Account
+                      </Link>
+                      <Link className="dropdown-item" to="/cart">
+                        Cart
+                      </Link>
+                      <div className="dropdown-item" onClick={this.signOut}>
+                        Logout
+                      </div>
+                    </div>
+                  </li>
+                </ul>
               </div>
-            </li>
-          </ul>
-          <div className="collapse navbar-collapse justify-content-end">
-            <div className="navbar-nav">
-              <ul className="navbar-nav">
-              <li className="nav-item dropdown dropleft">
-              <div
-                className="nav-link dropdown-toggle "
-                id="navbarDropdownMenuLink"
-                role="button"
-                data-toggle="dropdown"
-                aria-haspopup="true"
-                aria-expanded="false"
-              >
-                Tejas
-              </div>
-              <div
-                className="dropdown-menu"
-                aria-labelledby="navbarDropdownMenuLink"
-              >
-                <Link className="dropdown-item" to="/account">
-                  Account
-                </Link>
-                <Link className="dropdown-item" to="/cart">
-                  Cart
-                </Link>
-              </div>
-            </li>
-              </ul>
             </div>
-          </div>
+          ) : null}
         </div>
       </nav>
     );
   }
 }
 
-export default Navbar;
+const mapStateToProps = (state) => ({
+  user: state.auth.user,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  signOut: () => dispatch(authActions.signOut()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Navbar);
--- client/src/config/server.js
+++ client/src/config/server.js
@@ -1,4 +1,4 @@
 export default {
-  domain: "10.0.0.155",
+  domain: "10.0.0.47",
   port: "8080"
 };
